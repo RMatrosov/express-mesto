e224d22a62ff17481ef4c6514c33424eab362d3c
Roman Matrosov <69420192+RMatrosov@users.noreply.github.com>
2021-10-25 16:28:44 +0300

    add logger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const error = require('./middlewares/error');
 const NotFoundError = require('./errors/NotFoundError');
 
@@ -20,13 +21,14 @@ app.disable('x-powered-by');
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(helmet());
+app.use(requestLogger);
 app.use('/', require('./routes/users'));
-
 app.use('/', require('./routes/cards'));
 
 app.use('*', () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
+app.use(errorLogger);
 
 app.use(errors());
 
